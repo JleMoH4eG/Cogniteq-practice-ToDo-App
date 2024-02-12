65c5b4143aec873bc2268c965dfe441c352a589a
maxgen6 <62709031+maxgen6@users.noreply.github.com>
2024-02-12 08:41:33 +0100

    Update Input.jsx

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,6 +1,10 @@
 import classes from "./Input.module.scss";
 
 function Input({ inputId, value, getTaskTitleValue, style, ...props }) {
+  // need to change getTaskTitleValue to onChange
+  // const classNames = [classes.input]
+  // if (props.className) classNames.push(props.className) 
+  // in <input className={classNames.join(' ')}
   return (
     <input
       onChange={getTaskTitleValue}
